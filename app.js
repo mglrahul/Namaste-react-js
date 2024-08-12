61b9d4fb0100f9439efd6386c0dd752491607fd9
Rahul Mangal <mglrahul@gmail.com>
2024-08-13 05:21:45 +0530

    completed the chapter 3

--- app.js
+++ app.js
@@ -1,6 +1,32 @@
+// created a server
+// Hot module replacement - HMR
+// file watchers algo in c++
+// bundling
+// minify
+// cleaning our code
+// dev and prod build
+// super fast build algorithm
+// image optimization
+// caching while development
+// file compressions
+// compatible with older version of browsers
+// https on dev - npx parcel index.html --https
+// manage port number of many projects
+// consistent hashing algorithm to cache things up
+// zero configuration
+// transitive dependencies - all the sub dependency of different packages in package.json file
+// 
+
+
+
+
+
+import React from 'react';
+import ReactDOM from "react-dom/client";
+
 const heading =  React.createElement("h1", {
     id: "title"
-}, "Heading 1");
+}, "Heading 1 from parcel");
 
 const heading2 =  React.createElement("h1", {
     id: "title"
