49f0bdbf1bec8faeb5b6c1d13dc880d5d16becc3
Rahul Mangal <mglrahul@gmail.com>
2024-08-12 19:46:17 +0530

    complete the chapter 1 & 2

---
+++ app.js
@@ -0,0 +1,16 @@
+const heading =  React.createElement("h1", {
+    id: "title"
+}, "Heading 1");
+
+const heading2 =  React.createElement("h1", {
+    id: "title"
+}, "heading 2");
+
+const container = React.createElement("div", {
+    id: "container"
+}, [heading, heading2])
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(container);
